f664fafd309e8ca7682e1f15cffdf2522a358964
robertschaap <robert.schaap@mac.com>
2018-07-25 21:22:13 +0200

    Heading2: setup and styling of component

---
+++ components/Heading2.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Heading2 = styled.h2`
+  text-align: center;
+  font-weight: 200;
+  font-size: 2rem;
+  margin-top: 0;
+`;
+
+export default Heading2;
