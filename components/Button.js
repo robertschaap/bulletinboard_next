45df6b4f5c68a7f947b5bc73afb213dc16e42404
robertschaap <robert.schaap@mac.com>
2018-07-25 21:37:04 +0200

    Button: setup and styling of component

---
+++ components/Button.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-family: 'Avenir Next', 'Arial', sans-serif;
+  display: block;
+  border: none;
+  padding: 0.5rem;
+  background-color: #fff;
+  border-radius: 5px;
+  margin-top: 0.5rem;
+  width: 100%;
+`;
+
+export default Button;
