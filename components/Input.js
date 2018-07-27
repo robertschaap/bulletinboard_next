4b4cf44a5c637f111c97d5a10bc747ae845176ff
robertschaap <robert.schaap@mac.com>
2018-07-27 21:27:28 +0200

    Input: setup and styling of component

---
+++ components/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  font-family: 'Avenir Next', 'Arial', sans-serif;
+  display: block;
+  border: none;
+
+  width: 100%;
+  border-radius: 5px;
+  margin-bottom: 0.5rem;
+`;
+
+export default (props) => (
+  <Input {...props} />
+);
