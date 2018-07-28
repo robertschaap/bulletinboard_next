f0722566dc14ef2178d1099e78719c1471876086
robertschaap <robert.schaap@mac.com>
2018-07-28 13:26:29 +0200

    ReadSomething: intial setup of page

---
+++ pages/readsomething.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import Layout from "../components/Layout";
+import Heading2 from "../components/Heading2";
+import Comment from "../components/Comment";
+import Button from "../components/Button";
+import Select from "../components/Select";
+
+class ReadSomething extends Component {
+  static getInitialProps({ query }) {
+    return {
+      comments: query.comments
+    };
+  }
+
+  state = {
+    comments: this.props.comments,
+    sortDirection: "desc",
+    offset: 0,
+  }
+
+  render() {
+    return (
+      <Layout>
+        <section>
+          <Heading2>Read Something</Heading2>
+          <Select>
+            <option value="desc">Newest First</option>
+            <option value="asc">Oldest First</option>
+          </Select>
+          <Comment />
+        </section>
+        <section>
+          <Button>Load More Comments</Button>
+        </section>
+      </Layout>
+    );
+  }
+}
+
+export default ReadSomething;
