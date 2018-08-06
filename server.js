ac8584ac47566b88ec1fffd4acf6938f4519c5f2
robertschaap <robert.schaap@mac.com>
2018-08-06 21:20:12 +0200

    server.js: added crude comment model logic

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const next = require("next");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -8,23 +10,39 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(bodyParser.json());
+
+  mongoose.connect("mongodb://localhost:27017/bulletinboard");
+  mongoose.connection.on("error", console.error.bind(console), "MDB Connect Err"); //eslint-disable-line
+  mongoose.Promise = global.Promise;
+
+  const Comment = require("./models/comment");
 
   server.get("/", (req, res) => {
     return app.render(req, res, "/readsomething");
   });
 
   server.get("/readsomething", (req, res) => {
-    // TODO: replace dummy data with db data
-    const comments = [
-      { title: "A", body: "B", avatar: "bunny", name: "C" }
-    ];
+    // let { offset, sort } = req.query;
 
-    return app.render(req, res, "/readsomething", { comments });
+    Comment.find()
+      // .sort({ createdAt: sort })
+      .limit(4)
+      // .skip(+offset)
+      .then(result => {
+        return app.render(req, res, "/readsomething", { comments: result });
+      });
   });
 
   server.post("/writesomething", (req, res) => {
+
+    let { title, body, avatar, name } = req.body;
+
+    Comment.new(title, body, avatar, name)
+      .then(result => res.json(result));
     // TODO: wire to database
-    console.log(req, res); //eslint-disable-line
+    // console.log(req, res); //eslint-disable-line
   });
 
   server.get("*", (req, res) => {
