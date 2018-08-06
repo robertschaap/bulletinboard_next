8ab7e7edad922e165c45d7934d23814b33348254
robertschaap <robert.schaap@mac.com>
2018-08-06 21:17:23 +0200

    Setup CommentSchema

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = Schema({
+  title: String,
+  body: String,
+  avatar: String,
+  name: String
+}, { timestamps: true });
+
+CommentSchema.statics.new = (title, body, avatar, name) => {
+  let record = new Comment({
+    title,
+    body,
+    avatar,
+    name
+  });
+  return record.save();
+};
+
+const Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
