d245b192f7ecd6804a051e6ca1b70316667e9afc
robertschaap <robert.schaap@mac.com>
2018-08-11 19:29:53 +0200

    WriteSomething: implemented form submit logic

--- pages/writesomething.js
+++ pages/writesomething.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Router from "next/router";
 
 import Layout from "../components/Layout";
 import Heading2 from "../components/Heading2";
@@ -31,12 +32,25 @@ class WriteSomething extends Component {
     });
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(this.state.form)
+    };
+
+    fetch("/api/writesomething", options)
+      .then(Router.push("/readsomething"));
+  }
+
   render() {
     return (
       <Layout>
         <section>
           <Heading2>Write Something</Heading2>
-          <Form>
+          <Form onSubmit={this.handleSubmit}>
             <Label>What's your name?:</Label>
             <Input type="text" name="name" onChange={this.handleChange} />
             <Label>Write a title (max 100 characters):</Label>
@@ -53,7 +67,7 @@ class WriteSomething extends Component {
               <option value="puppy">A bashful puppy</option>
               <option value="sheep">The master race</option>
             </Select>
-            <Button>Submit</Button>
+            <Button type="submit">Submit</Button>
           </Form>
         </section>
       </Layout>
