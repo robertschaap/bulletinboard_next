b0ce518a5f5dd20863d4bab4ab41f2c326871452
robertschaap <robert.schaap@mac.com>
2018-08-12 14:00:10 +0200

    Comment export refactoring

--- components/Comment.js
+++ components/Comment.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import Heading3 from "./Heading3";
 import Heading4 from "./Heading4";
 
-const Comment = styled.div`
+const CommentWrapper = styled.div`
   margin-bottom: 1rem;
   background-color: rgba(230,255,245,0.5);
 
@@ -33,13 +33,13 @@ const CommentImage = styled.img`
   border-radius: 50%;
 `;
 
-export default ({ avatar, body, name, title }) => {
+const Comment = ({ avatar, body, name, title }) => {
   if (!avatar || !body || !name || !title) {
     return null;
   }
 
   return (
-    <Comment>
+    <CommentWrapper>
       <CommentInfo>
         <CommentImage src={`/static/images/${avatar}.png`} alt={avatar} width="100%" />
       </CommentInfo>
@@ -48,6 +48,8 @@ export default ({ avatar, body, name, title }) => {
         <Heading4>{name}</Heading4>
         <p>{body}</p>
       </CommentBody>
-    </Comment>
+    </CommentWrapper>
   );
 };
+
+export default Comment;
